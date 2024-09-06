c0b9727d42592f426e51e6e3848eb4c778ecd246
waleed-awan <waleedcine007@gmail.com>
2024-09-06 13:02:47 +0500

    Add project Link in navbar and remove contact link

--- src/app/components/navbar/page.jsx
+++ src/app/components/navbar/page.jsx
@@ -8,7 +8,6 @@ import { gsap } from "gsap";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -22,14 +21,6 @@ const Navbar = () => {
   const handleLinkClick = () => {
     setIsOpen(false);
     document.body.style.overflow = "auto";
-  };
-
-  const handleDropdownEnter = () => {
-    setIsDropdownOpen(true);
-  };
-
-  const handleDropdownLeave = () => {
-    setIsDropdownOpen(false);
   };
 
   useEffect(() => {
@@ -47,26 +38,6 @@ const Navbar = () => {
       document.body.style.overflow = "auto";
     };
   }, []);
-
-  useEffect(() => {
-    if (isDropdownOpen) {
-      gsap.to(".dropdown-menu", {
-        duration: 0.5,
-        opacity: 1,
-        y: 0,
-        display: "block",
-        ease: "power2.out",
-      });
-    } else {
-      gsap.to(".dropdown-menu", {
-        duration: 0.5,
-        opacity: 0,
-        y: -20,
-        display: "none",
-        ease: "power2.in",
-      });
-    }
-  }, [isDropdownOpen]);
 
   return (
     <nav
@@ -106,12 +77,23 @@ const Navbar = () => {
         <div
           className={`fixed inset-0 bg-[#0D1B1B] bg-opacity-90 z-10 flex flex-col items-center justify-center space-y-4 text-white transform ${isOpen ? "translate-x-0 h-screen" : "translate-x-full h-0"} transition-transform duration-300 md:relative md:bg-transparent md:inset-auto md:flex md:flex-row md:space-y-0 md:space-x-4 md:translate-x-0 md:h-auto`}
         >
+          {/* Home Link */}
           <Link href="/" passHref>
             <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-1 relative group">
               Home
               <span className="absolute left-1/2 transform -translate-x-1/2 bottom-0 h-0.5 w-0 bg-[#2DD4BF] transition-all duration-500 group-hover:w-full mt-2"></span>
             </div>
           </Link>
+
+          {/* Projects Link */}
+          <Link href="/Projects" passHref>
+            <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-1 relative group">
+              Projects
+              <span className="absolute left-1/2 transform -translate-x-1/2 bottom-0 h-0.5 w-0 bg-[#2DD4BF] transition-all duration-500 group-hover:w-full mt-2"></span>
+            </div>
+          </Link>
+
+          {/* Services Link */}
           <Link href="/services" passHref>
             <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-1 relative group">
               Services
@@ -119,53 +101,48 @@ const Navbar = () => {
             </div>
           </Link>
 
-          <div
-            className="relative"
-            onMouseEnter={handleDropdownEnter}
-            onMouseLeave={handleDropdownLeave}
-          >
-            <Link href="/about" passHref>
-              <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-2 relative group">
-                About <span className="text-teal-500 text-2xl">+</span>
-                <span className="absolute left-1/2 transform -translate-x-1/2 bottom-0 h-0.5 w-0 bg-[#2DD4BF] transition-all duration-500 group-hover:w-full mt-2"></span>
-              </div>
-            </Link>
-            <div className="dropdown-menu absolute left-1/2 -translate-x-1/2 top-full mt-2 bg-[#0D1B1B] p-4 rounded-lg shadow-lg opacity-0 transform -translate-y-2 z-20 hidden md:block">
-              <Link href="/team" passHref>
-                <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-1 relative group">
-                  Team
-                  <span className="absolute left-1/2 transform -translate-x-1/2 bottom-0 h-0.5 w-0 bg-[#2DD4BF] transition-all duration-500 group-hover:w-full mt-2"></span>
-                </div>
-              </Link>
-              <Link href="/career" passHref>
-                <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-1 relative group">
-                  Career
-                  <span className="absolute left-1/2 transform -translate-x-1/2 bottom-0 h-0.5 w-0 bg-[#2DD4BF] transition-all duration-500 group-hover:w-full mt-2"></span>
-                </div>
-              </Link>
+          {/* About Link */}
+          <Link href="/about" passHref>
+            <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-1 relative group">
+              About
+              <span className="absolute left-1/2 transform -translate-x-1/2 bottom-0 h-0.5 w-0 bg-[#2DD4BF] transition-all duration-500 group-hover:w-full mt-2"></span>
             </div>
-          </div>
+          </Link>
+          {/* Team Link*/}
+          <Link href="/team" passHref>
+            <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-1 relative group">
+              Team
+              <span className="absolute left-1/2 transform -translate-x-1/2 bottom-0 h-0.5 w-0 bg-[#2DD4BF] transition-all duration-500 group-hover:w-full mt-2"></span>
+            </div>
+          </Link>
+          {/* Career Link*/}
+          <Link href="/career" passHref>
+            <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-1 relative group">
+              Career
+              <span className="absolute left-1/2 transform -translate-x-1/2 bottom-0 h-0.5 w-0 bg-[#2DD4BF] transition-all duration-500 group-hover:w-full mt-2"></span>
+            </div>
+          </Link>
+
+          {/* Blog Link */}
           <Link href="/blogs" passHref>
             <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-1 relative group">
               Blog
               <span className="absolute left-1/2 transform -translate-x-1/2 bottom-0 h-0.5 w-0 bg-[#2DD4BF] transition-all duration-500 group-hover:w-full mt-2"></span>
             </div>
           </Link>
-          <Link href="/contact" passHref>
-            <div onClick={handleLinkClick} className="hover:text-[#2DD4BF] cursor-pointer pb-1 relative group">
-              Contact
-              <span className="absolute left-1/2 transform -translate-x-1/2 bottom-0 h-0.5 w-0 bg-[#2DD4BF] transition-all duration-500 group-hover:w-full mt-2"></span>
-            </div>
-          </Link>
+
+          {/* Get Started Button for Mobile */}
           <Link href="/mission" passHref>
             <div onClick={handleLinkClick} className="text-[#2DD4BF] border-2 border-[#2DD4BF] py-2 px-4 rounded hover:bg-[#2DD4BF] hover:text-gray-900 transition cursor-pointer md:hidden">
-              Get Started
+              Get In Touch
             </div>
           </Link>
         </div>
-        <Link href="/" passHref>
+
+        {/* Get Started Button for Desktop */}
+        <Link href="/contact" passHref>
           <div onClick={handleLinkClick} className="text-[#2DD4BF] border-2 border-[#2DD4BF] py-2 px-4 hover:bg-[#2DD4BF] hover:text-gray-900 transition cursor-pointer hidden md:block">
-            Get Started
+            Get In Touch
           </div>
         </Link>
       </div>
